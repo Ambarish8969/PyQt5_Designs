123496807c53897281a19391a0a52a599a3df169
Ambarish <ambarish8653@gmail.com>
2021-09-09 17:55:43 +0530

    Updated.

--- calculator.py
+++ calculator.py
@@ -5,26 +5,37 @@ print(" A is Addition.\n B is Subtraction.\n C is Multiplication.\n D is Divisio
 
 choice = input("Enter your choice A/B/C/D.? : ").lower()
 
-def add(num1,num2):
-    sum=num1+num2
+
+def add(num1, num2):
+    sum = num1+num2
     return sum
-def sub(num1,num2):
-    subr=num1-num2
+
+
+def sub(num1, num2):
+    subr = num1-num2
     return subr
-def mul(num1,num2):
-    mult=num1*num2
+
+
+def mul(num1, num2):
+    mult = num1*num2
     return mult
-def div(num1,num2):
-    divd=num1/num2
+
+
+def div(num1, num2):
+    divd = num1/num2
     return divd
 
+
 if choice == "a":
-    print(add(num1,num2))
+    print(add(num1, num2))
 elif(choice == "b"):
-    print(sub(num1,num2))
+    print(sub(num1, num2))
 elif(choice == "c"):
-    print(mul(num1,num2))
+    print(mul(num1, num2))
 elif(choice == "d"):
-    print(div(num1,num2))
+    print(div(num1, num2))
 else:
     print("Invalid.")
+
+
+print("hello world")
