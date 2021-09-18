3ad63841ca108d62cd35b08a3d13ef868a96a939
Ambarish <ambarish8653@gmail.com>
2021-09-18 18:54:26 +0530

    Updated.

--- try2.py
+++ try2.py
@@ -1,30 +1,27 @@
 # Firstly we have to import 'xml.etree.ElementTree' for creating a subtree
 import xml.etree.ElementTree as ET
-import os
   
 # users_list = ["GeeksForGeeks", "Arka", "Computer Science", "Engineering", "Portal"]
 f=open("try.txt","r")
 r=f.read()
-print(r)
-
 
 def create_xml():
   
-        # we make root element
-        usrconfig = ET.Element("usrconfig")
-  
-        # create sub element
-        usrconfig = ET.SubElement(usrconfig, "usrconfig")
-  
-        # insert list element into sub elements
-        for user in range(len( r)):
-  
-                usr = ET.SubElement(usrconfig, "usr")
-                usr.text = str(r[user])
-  
-        tree = ET.ElementTree(usrconfig)
-  
-        # write the tree into an XML file
-        tree.write("Output.xml", encoding ='utf-8', xml_declaration = True)
+    # we make root element
+    usrconfig = ET.Element("usrconfig")
+
+    # create sub element
+    usrconfig = ET.SubElement(usrconfig, "usrconfig")
+
+    # insert list element into sub elements
+    for user in range(len( r)):
+
+        usr = ET.SubElement(usrconfig, "usr")
+        usr.text = str(r[user])
+
+    tree = ET.ElementTree(usrconfig)
+
+    # write the tree into an XML file
+    tree.write("Output.xml", encoding ='utf-8', xml_declaration = True)
   
 create_xml()
